f6b3eade398e4d096aa451cb1e785f661f7f862b
zhixiang <zhixiang96@hotmail.com>
2017-04-24 15:43:41 +0800

    update

--- login.php
+++ login.php
@@ -48,51 +48,7 @@ if (!isPJAX()): ?>
             <input class="loginBut" name="blog_login_request" type="submit" value="Done">
         </form>
     </div>
-    <div id="footer">
-        <div class="footer_tips">
-            <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/cn/"><img alt="知识共享许可协议" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-nd/3.0/cn/80x15.png" /></a><br />本博客作品均采用<a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/cn/">知识共享署名-非商业性使用-禁止演绎 3.0 中国大陆许可协议</a>进行许可。
-            <p>* www.huaji.io是huaji.io的SSL安全副本，且huaji.io仍处于测试阶段</p>
-        </div>
-        <div class="footer_block" style="margin-left: 10%">
-            <p class="footer_block_title">Authority</p>
-            <ul class="footer_block_list">
-                <li><a href="login.php">Login</a></li>
-                <li><a href="new.php">New page</a></li>
-                <li><a href="upload.php">Upload</a></li>
-                <li><a href="status.php">Status</a></li>
-            </ul>
-        </div>
-        <div class="footer_block">
-            <p class="footer_block_title">Contact me</p>
-            <ul class="footer_block_list">
-                <li><a href="mailto:zhixiang96@hotmail.com">E-mail</a></li>
-                <li><a href="https://github.com/zhixiang96">Github</a></li>
-            </ul>
-        </div>
-        <div class="footer_block">
-            <p class="footer_block_title">Category</p>
-            <ul class="footer_block_list">
-                <li><a href="category.php?=spirits">Spirits</a></li>
-                <li><a href="category.php?=points">Points</a></li>
-                <li><a href="category.php?=code">Code</a></li>
-                <li><a href="category.php?=trick">Trick</a></li>
-            </ul>
-        </div>
-        <div class="footer_block">
-            <p class="footer_block_title">Partner sites</p>
-            <ul class="footer_block_list">
-                <li><a href="http://cn.naivefrog.win">naivefrog's Blog</a></li>
-                <li><a href="https://www.moekr.com">Moekr's Blog</a></li>
-                <li><a href="https://www.belang.org">belang.org</a></li>
-                <li><a href="http://bella.huaji.io">Bella</a></li>
-            </ul>
-        </div>
-        <div id="footer_end" style="text-align: center">
-            <div style="position: absolute;left: 0">Copyright © 2017 huaji.io All rights reserved.</div>
-            <div style="position: absolute;left: 45%">鲁ICP备16011857号-2</div>
-            <div style="position: absolute;right: 0">powered by <a href="http://bella.huaji.io">Bella</a></div>
-        </div>
-    </div>
+    <?php include "footer.php"?>
     <script type="text/javascript" src="js/login.js"></script>
 
     <?php if (!isPJAX()): ?>
